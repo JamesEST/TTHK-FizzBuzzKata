331611f77fbd52f0e1b250e53da9ad7609fe98e1
Vladislav <vladislav.narozni@outlook.com>
2021-10-20 12:39:05 +0300

    Returned back 0 in the test to make it work

--- src/tthk/narozni/tarpv19/FizzBuzzTest.java
+++ src/tthk/narozni/tarpv19/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void shouldReturn0() {
-        assertEquals(FizzBuzz.of(0), ("2"));
+        assertEquals(FizzBuzz.of(0), ("0"));
     }
     @Test
     public void shouldReturn1(){
