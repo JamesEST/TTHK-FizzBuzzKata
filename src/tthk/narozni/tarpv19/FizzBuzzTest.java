ebac985b4796ad0878fed7a267ec95a3e3696dc7
Vladislav <vladislav.narozni@outlook.com>
2021-10-20 12:38:24 +0300

    I checked what would happen if I put another number in the test with 0

--- src/tthk/narozni/tarpv19/FizzBuzzTest.java
+++ src/tthk/narozni/tarpv19/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void shouldReturn0() {
-        assertEquals(FizzBuzz.of(0), ("0"));
+        assertEquals(FizzBuzz.of(0), ("2"));
     }
     @Test
     public void shouldReturn1(){
