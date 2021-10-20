8ffe410843e717c31ff213e887307f3c44363a3b
Vladislav <vladislav.narozni@outlook.com>
2021-10-20 12:29:00 +0300

    Create test for return "0"

--- src/tthk/narozni/tarpv19/FizzBuzzTest.java
+++ src/tthk/narozni/tarpv19/FizzBuzzTest.java
@@ -1,4 +1,11 @@
 package tthk.narozni.tarpv19;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void shouldReturn0() {
+        assertEquals(FizzBuzz.of(0), ("0"));
+    }
 }
