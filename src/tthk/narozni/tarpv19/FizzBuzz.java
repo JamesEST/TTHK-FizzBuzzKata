418e3a113dcefa26253bdb37bdfcafeee81fb424
Vladislav <vladislav.narozni@outlook.com>
2021-10-25 10:44:41 +0300

    Project rebuild and add main function

--- src/tthk/narozni/tarpv19/FizzBuzz.java
+++ src/tthk/narozni/tarpv19/FizzBuzz.java
@@ -6,11 +6,9 @@ public class FizzBuzz {
             System.out.println(of(i));
         }
     }
+
     public static String of(int i) {
-        if(i == 0) return "0";
-        if(i % 5 == 0 && i % 3 == 0) return "FizzBuzz";
-        if(i % 3 == 0) return "Fizz";
-        if(i % 5 == 0) return "Buzz";
-        return String.valueOf(i);
+        return "0";
     }
+
 }
