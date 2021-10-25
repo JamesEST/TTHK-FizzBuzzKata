1f21ab2af273aeb87cb28a13935fc70ada71647a
Vladislav <vladislav.narozni@outlook.com>
2021-10-25 12:05:34 +0300

    Fix test for return "0"

--- src/tthk/narozni/tarpv19/FizzBuzz.java
+++ src/tthk/narozni/tarpv19/FizzBuzz.java
@@ -8,6 +8,7 @@ public class FizzBuzz {
     }
 
     public static String of(int i) {
+        if(i == 0) return "0";
         if(i % 5 == 0 && i % 3 == 0) return "FizzBuzz";
         if(i % 3 == 0) return "Fizz";
         if(i % 5 == 0) return "Buzz";
